5e4417156a5eb19ea734aa9d32c322e75dfa2c3f
Tuong <ttruong2621@sdsu.edu>
2026-05-25 15:35:56 -0700

    Adding first get for theuser service

---
+++ backend/src/app/services/user.py
@@ -0,0 +1,20 @@
+from app.repository.user import UserRepository
+from app.schemas.user import UserResponse
+from fastapi import HTTPEXception, status
+from sqlaalchemy.orm import Session
+
+
+class userService:
+    def __init__(self) -> None:
+        self.repository = UserRepository()
+
+    "Get"
+
+    def get_by_mail(self, db: Session, email: str) -> UserResponse | None:
+        user_email = self.repository.get_by_mail(db, email)
+        if user_email is None:
+            raise HTTPEXception(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="User not found",
+            )
+        return user_email
