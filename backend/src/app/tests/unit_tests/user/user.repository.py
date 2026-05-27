cb7cffeae1ab02da3c71dae925fb904962f27249
Tuong <ttruong2621@sdsu.edu>
2026-05-27 15:22:14 -0700

    Creating testing for delete user

--- backend/src/app/tests/unit_tests/user/user.repository.py
+++ backend/src/app/tests/unit_tests/user/user.repository.py
@@ -14,3 +14,16 @@ def test_create_user(db: Session):
     assert user.id is not None
     assert user.email == "test@example.com"
     assert user.username == "testuser"
+
+
+def test_delete_user(db: Session):
+    user_db = UserDB(
+        username="testuser",
+        email="test@example.com",
+        hashed_password="hashedpassword123",
+    )
+    created = UserRepository.create_user(db, user_db)
+    UserRepository.delete_user(db, created)
+
+    user = UserRepository.get_by_mail(db, "test@example.com")
+    assert user is None
