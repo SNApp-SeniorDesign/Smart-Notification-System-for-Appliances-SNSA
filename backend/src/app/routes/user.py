4bf1444c0297954a5b3a8c60cc7b7237bdb2088f
Tuong <ttruong2621@sdsu.edu>
2026-05-27 14:48:54 -0700

    Finish delete endpoint

--- backend/src/app/routes/user.py
+++ backend/src/app/routes/user.py
@@ -5,10 +5,14 @@ from typing import Annotated
 from app.services.user import user_service
 from app.schemas.user import UserResponse as UserSchema
 from app.schemas.user import UserCreate
+from app.models.user import User as UserModel
+from app.repository.user import UserRepository as user_repository
 
 from app.core.database import get_db
 
 api_router = APIRouter(prefix="/users", tags=["users"])
+
+"Post"
 
 
 @api_router.post(
@@ -18,3 +22,14 @@ async def register_user(
     user: UserCreate, db: Annotated[Session, Depends(get_db)]
 ) -> UserSchema:
     return user_service.register_user(db, user)
+
+
+"Delete"
+
+
+@api_router.delete("/me", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(
+    db: Annotated(Session, Depends(get_db)),
+    current_user: Annotated(UserModel, Depends(user_service.get_current_user)),
+) -> None:
+    user_repository.delete_user(db, current_user)
