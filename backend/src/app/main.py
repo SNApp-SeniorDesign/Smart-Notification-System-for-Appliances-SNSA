9bf54bee845c42de535465b6d39a017722f41be9
Tuong <ttruong2621@sdsu.edu>
2026-06-08 15:09:43 -0700

    Modify all integrating test in a way it work by itself

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -74,6 +74,3 @@ async def eventTrigger(event_message: EventMessage):
 @app.get("/latest-event")
 async def get_latest_event():
     return latest_event
-
-
-print("DATABASE_URL:", settings.database_url)
