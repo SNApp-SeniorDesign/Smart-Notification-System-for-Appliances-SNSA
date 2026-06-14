b983acfdd1d198d37f31b62a36babd1242394a89
Tuong <ttruong2621@sdsu.edu>
2026-06-13 18:36:24 -0700

    Adding test for user service when get exception with none get by mail

--- backend/src/app/tests/unit_tests/user/user_services_test.py
+++ backend/src/app/tests/unit_tests/user/user_services_test.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import Mock
 from app.services.user import UserService
+from fastapi import HTTPException
 
 
 @pytest.fixture
@@ -15,8 +16,22 @@ def db():
     return Mock()
 
 
+"Get"
+
+
 def test_get_by_mail_success(service, db):
     fake_user = Mock(email="test@example.com")
     service.repository.get_by_mail.return_value = fake_user
     result = service.get_by_mail(db, "test@example.com")
     assert result == fake_user
+
+
+def test_get_by_mail_not_found(service, db):
+    service.repository.get_by_mail.return_value = None
+
+    with pytest.raises(HTTPException) as exc_info:
+        service.get_by_mail(db, "notexist@example.com")
+
+    service.repository.get_by_mail.assert_called_once_with(db, "notexist@example.com")
+
+    assert exc_info.value.status_code == 404
