67e13aca807c09bbee84b3c984ca5b6b93cfaa7a
Tuong <ttruong2621@sdsu.edu>
2026-06-15 17:42:50 -0700

    Adding test to check email is taken success for service

--- backend/src/app/tests/unit_tests/user/user_services_test.py
+++ backend/src/app/tests/unit_tests/user/user_services_test.py
@@ -16,14 +16,15 @@ def db():
     return Mock()
 
 
+fake_user_email = Mock(email="test@example.com")
+fake_user_id = Mock(id=1)
 "Get"
 
 
 def test_get_by_mail_success(service, db):
-    fake_user = Mock(email="test@example.com")
-    service.repository.get_by_mail.return_value = fake_user
+    service.repository.get_by_mail.return_value = fake_user_email
     result = service.get_by_mail(db, "test@example.com")
-    assert result == fake_user
+    assert result == fake_user_email
 
 
 def test_get_by_mail_not_found(service, db):
@@ -38,10 +39,9 @@ def test_get_by_mail_not_found(service, db):
 
 
 def test_get_by_id_success(service, db):
-    fake_user = Mock(id=1)
-    service.repository.get_by_id.return_value = fake_user
+    service.repository.get_by_id.return_value = fake_user_id
     result = service.get_by_id(db, 1)
-    assert result == fake_user
+    assert result == fake_user_id
 
 
 def test_get_by_id_not_found(service, db):
@@ -53,3 +53,10 @@ def test_get_by_id_not_found(service, db):
     service.repository.get_by_id.assert_called_once_with(db, 99999)
 
     assert exc_info.value.status_code == 404
+
+
+def test_is_email_taken(service, db):
+    service.repository.is_email_taken.return_value = object()
+    result = service.is_email_taken(db, "test@example.com")
+    assert result is True
+    service.repository.get_by_mail.assert_called_once_with(db, "test@example.com")
