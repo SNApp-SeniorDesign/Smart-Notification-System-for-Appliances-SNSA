0f9bbac1409a454586c6507bd1da26b3cdd3c591
Tuong <ttruong2621@sdsu.edu>
2026-06-19 17:56:12 -0700

    Finish devices schema

--- backend/src/app/schemas/devices.py
+++ backend/src/app/schemas/devices.py
@@ -1,6 +1,44 @@
 from pydantic import Field, BaseModel
+from typing import Literal
+
+DeviceStatus = Literal["Offline", "Online", "Pairing", "Waiting"]
 
 
 class DeviceBase(BaseModel):
+    "Base that contain all repeated element"
+
+    device_name: str = Field(min_length=3)
+
+
+class DeviceCreate(DeviceBase):
+    "Input accepted from the client when creating a device"
+
+    serial_number: str
+
+
+class DeviceResponse(DeviceCreate):
+    "Data returned to the client when a device is requested"
+
+    id: int
     user_id: int
-    device_name: str = Field(min_lenght=3)
+    serial_number: str
+    device_status: DeviceStatus
+    is_paired: bool
+
+    model_config = {"from_attributes": True}
+
+
+class DeviceDB(DeviceCreate):
+    "Internal schema used by backend before saving to database"
+
+    user_id: int
+    is_paired: bool = False
+    device_status: DeviceStatus = "offline"
+
+
+class DeviceUpdate(BaseModel):
+    "Fields that are allowed to be modified after device creation"
+
+    device_name: str | None = None
+    device_status: DeviceStatus | None = None
+    is_paired: bool | None = None
