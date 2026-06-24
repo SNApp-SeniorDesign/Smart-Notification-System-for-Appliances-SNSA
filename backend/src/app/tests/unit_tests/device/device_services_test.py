e3edc10219aea56d0d2e29e24776a368a2c9229e
Tuong <ttruong2621@sdsu.edu>
2026-06-24 15:21:02 -0700

    Adding success test case for is serial number taken service

--- backend/src/app/tests/unit_tests/device/device_services_test.py
+++ backend/src/app/tests/unit_tests/device/device_services_test.py
@@ -29,3 +29,12 @@ def test_is_device_name_taken_fail(service, db):
     service.repository.get_by_device_name.return_value = None
     result = service.is_device_name_taken(db, "TestDevice")
     assert result is False
+
+
+def test_is_serial_name_taken_success(service, db):
+    service.repository.get_by_serial_name.return_value = object()
+    result = service.is_serial_number_taken(db, "testSerialNumber123")
+    assert result is True
+    service.repository.get_by_serial_number.assert_called_once_with(
+        db, "testSerialNumber123"
+    )
