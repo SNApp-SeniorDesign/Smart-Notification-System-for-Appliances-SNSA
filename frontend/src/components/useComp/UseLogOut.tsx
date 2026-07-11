9fb711c9f245af5ef541c6c334ab89b7cf487817
Tuong <ttruong2621@sdsu.edu>
2026-07-11 14:49:02 -0700

    Create log out button

---
+++ frontend/src/components/useComp/UseLogOut.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import * as React from "react"
+import { toast } from "sonner"
+import { Button } from "@/components/ui/button"
+import { getToken, clearToken} from "@/lib/auth"
+
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+async function handleLogOut() {
+    const token = getToken()
+
+    try {
+        const res = await fetch(`${API_URL}/users/logout`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`,
+            }
+        })
+        if (res.ok) {
+            toast.success("Log Out Successfully", {position: "top-center"})
+        } else {
+            toast.message("Logged out", { position: "top-center"})
+        }
+    } catch {
+        toast.message("Logged out", { position: "top-center"})
+    }
+    clearToken()
+    window.location.href = "/"
+}
+
+export function LogOutButton(){
+    return (
+        <Button variant="destructive" onClick={handleLogOut}>
+            Log-Out
+        </Button>
+    )
+}
