a17526d8109f94a0708f754d02c8ac325e2a3ade
Tuong <ttruong2621@sdsu.edu>
2026-07-17 18:01:50 -0700

    Get rid of the delete button and replace it with dialog version instead

---
+++ frontend/src/components/useComp/device/UseDialogDeleteDevice.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { toast } from "sonner"
+import { getToken } from "@/lib/auth"
+import { Button } from "@/components/ui/button"
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+type DeleteDeviceDialogProps = {
+    deviceID: number
+}
+
+export function AlertDialogDemo({
+    deviceID,
+}: DeleteDeviceDialogProps) {
+
+    async function handleDeleteDevice() {
+    
+    const token = getToken()
+
+    try {
+      const res = await fetch(`${API_URL}/device/${deviceID}/delete`, {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      if (!res.ok) {
+        toast.error("Failed to delete device. Please try again later.", {
+          position: "top-center",
+        })
+        return
+      }
+
+     
+      toast.success("Device deleted successfully", {
+        position: "top-center",
+      })
+
+    } catch {
+      toast.error("Failed to delete device. Please try again later.", {
+        position: "top-center",
+      })
+    }
+  }
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger render={
+        <Button variant="destructive"> Delete Device </Button>
+        } />
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            device from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={handleDeleteDevice}
+          >Delete Device</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
