f3ba2218b4f6edf5460733b4b309bb283fe5e7df
Tuong <ttruong2621@sdsu.edu>
2026-07-31 19:53:20 -0700

    Initialize the dasboard page with sound list body

--- frontend/src/app/(auth)/dashboard/page.tsx
+++ frontend/src/app/(auth)/dashboard/page.tsx
@@ -1,12 +1,28 @@
 "use client"
-
+import * as React from "react"
 import { CollapsibleDeviceMenu } from "@/components/useComp/device/UseDeviceMenu"
+import { SoundList } from "@/components/useComp/sound/UseSoundList"
+import {type Sound} from "@/components/useComp/sound/UseSoundCard"
+import {useDashboardContext} from "@/components/useComp/general/DashboardContext"
 
 
 export default function Dashboard(){
+    const {selectedDevice} = useDashboardContext()
+    const [selectedSound, setSelectedSound] = React.useState<Sound | null>(null)
+    function handleSoundClick(sound: Sound){
+        setSelectedSound(sound)
+    }
     return(
         <div>
             <CollapsibleDeviceMenu />
+                {selectedDevice ? (
+                    <SoundList
+                        deviceID={selectedDevice.id}
+                        onSoundClick={handleSoundClick}
+                    />
+                ) : (
+                    <p>Select a device to view its sounds.</p>
+                )}
         </div>
     )
 }
