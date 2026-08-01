444e73718a0314c445fe255364d4b4c796b1cd4c
Tuong <ttruong2621@sdsu.edu>
2026-07-31 18:22:24 -0700

    Modify footer to contain add sound dialog

--- frontend/src/components/useComp/general/UseFooter.tsx
+++ frontend/src/components/useComp/general/UseFooter.tsx
@@ -1,17 +1,40 @@
 "use client"
 
 import Link from "next/link"
+import {UseDialogAddSound} from "@/components/useComp/sound/UseDialogAddSound"
 
-export function Footer(){
+
+type FooterProps = {
+    open: boolean
+    onOpenChange: (open: boolean) => void
+    showTrigger?: boolean
+    deviceID: number
+    deviceSerialNumber: string
+}
+
+export function Footer({
+    open,
+    onOpenChange,
+    showTrigger = true,
+    deviceID,
+    deviceSerialNumber,
+}: FooterProps) {
     return(
         <footer>
-            <div className="flex items-center gap-4 w-full h-auto">
+            <div className="flex items-center gap-4 w-full h-auto justify-center">
                 <Link href="/dashboard">
                     Home
                 </Link>
                 <Link href="/setting">
                     Setting
                 </Link>
+                <UseDialogAddSound 
+                    deviceID={deviceID} 
+                    deviceSerialNumber={deviceSerialNumber}
+                    open={open}
+                    onOpenChange={onOpenChange}
+                    showTrigger={showTrigger}
+                />
             </div>
         </footer>
     )
