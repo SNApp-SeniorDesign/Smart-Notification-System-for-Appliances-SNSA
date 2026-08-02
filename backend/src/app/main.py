a3c751a773c1919421cca01fc6d969d8a5c2e3cb
Tuong <ttruong2621@sdsu.edu>
2026-08-01 19:40:02 -0700

    Fixing so the web site allow communication between backend and frontend during testing

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -36,10 +36,7 @@ app.include_router(recording_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        NEXT_URL,
-    ],
+    allow_origins=["http://localhost:3000", NEXT_URL, "http://192.168.0.187:3000"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
