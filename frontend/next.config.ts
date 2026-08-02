0d875ff4bdb49312b63937802b638e0692e62c0c
Tuong <ttruong2621@sdsu.edu>
2026-08-01 19:39:48 -0700

    Fixing so the web site allow communication between backend and frontend during testing

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "standalone",
+  allowedDevOrigins: [
+    "192.168.0.187",
+  ]
 };
 
 export default nextConfig;
