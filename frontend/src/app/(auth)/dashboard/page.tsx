f062df0c5de4d46dde180aac1e9ebf7e0b4448f3
Tuong <ttruong2621@sdsu.edu>
2026-08-01 18:41:59 -0700

    Modify dashboard page to keep track variable needed for other components

--- frontend/src/app/(auth)/dashboard/page.tsx
+++ frontend/src/app/(auth)/dashboard/page.tsx
@@ -5,11 +5,13 @@ import { SoundList } from "@/components/useComp/sound/UseSoundList"
 import {type Sound} from "@/components/useComp/sound/UseSoundCard"
 import {useDashboardContext} from "@/components/useComp/general/DashboardContext"
 import { DialogSoundForm } from "@/components/useComp/sound/UseDialogSoundMenu"
+import { ZodNull } from "zod/v3"
 
 export default function Dashboard(){
     const {selectedDevice} = useDashboardContext()
     const [selectedSound, setSelectedSound] = React.useState<Sound | null>(null)
     const [soundDialogOpen, setSoundDialogOpen] = React.useState(false)
+    const [soundRefreshKey, setSoundRefreshKey] = React.useState(0)
     function handleSoundClick(sound: Sound){
         setSelectedSound(sound)
         setSoundDialogOpen(true)
@@ -25,11 +27,16 @@ export default function Dashboard(){
                     }
                 }}
                 sound={selectedSound}
+                onDeleteSuccess={() => {
+                    setSoundRefreshKey((current) => current + 1)
+                    setSelectedSound(null)
+                }}
             />
                 {selectedDevice ? (
                     <SoundList
                         deviceID={selectedDevice.id}
                         onSoundClick={handleSoundClick}
+                        refreshKey={soundRefreshKey}
                     />
                 ) : (
                     <p>Select a device to view its sounds.</p>
