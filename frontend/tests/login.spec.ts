e1d126f14c36eedc72d2d1ea15d160613b1a4fb7
Tuong <ttruong2621@sdsu.edu>
2026-08-02 18:20:05 -0700

    Comfirm all old test work as intended

--- frontend/tests/login.spec.ts
+++ frontend/tests/login.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect} from "@playwright/test"
+import { test, expect } from "@playwright/test"
 import {signUp, Delete, MakeUser, LogOut} from "./auths"
 
 
@@ -6,29 +6,36 @@ test("user can log in and reach dashboard", async ({ page }) => {
 
     await page.goto("/")
     const user = MakeUser();
-    await signUp(page, user)
-    await LogOut(page, user)
-    await expect(page).toHaveURL(/\/$/)
-    
-    await expect(page.getByRole("button", {name:"Log In"})).toBeVisible()
-    
 
-    await page.getByRole("button", { name: "Log In"}).click()
+    try {
+        await signUp(page, user)
+        await LogOut(page, user)
+        await expect(page).toHaveURL(/\/$/)
+        
+        await expect(page.getByRole("button", {name:"Log In"})).toBeVisible()
+        
 
-    await expect(page.getByRole("dialog")).toBeVisible()
-    
-    await expect(
-      page.getByRole("heading", { name: "Log in to your Account" })
-    ).toBeVisible()
+        await page.getByRole("button", { name: "Log In"}).click()
 
-    await page.getByLabel("Email").fill(user.email)
-    await page.getByLabel("Password").fill(user.password)
-    await page.getByRole("button", { name: "Log In" }).click()
+        await expect(page.getByRole("dialog")).toBeVisible()
+        
+        await expect(
+        page.getByRole("heading", { name: "Log in to your Account" })
+        ).toBeVisible()
 
-    await expect(page).toHaveURL(/dashboard/)
-    const token = await page.evaluate(() =>
-        localStorage.getItem("access_token"))
-    expect(token).not.toBeNull()
+        await page.getByLabel("Email").fill(user.email)
+        await page.getByLabel("Password").fill(user.password)
 
-    await Delete(page, user)
+        await page.getByRole("button", { name: "Log In" }).click()
+
+        await expect(page).toHaveURL(/dashboard/)
+        const token = await page.evaluate(() =>
+            localStorage.getItem("access_token"))
+        expect(token).not.toBeNull()
+    } finally {
+        await Delete(page, user)
+    }
+
+
+
 })
