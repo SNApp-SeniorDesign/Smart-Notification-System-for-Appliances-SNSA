3d5a852f50da7ed78e029e2cbeb6bf061556c8f7
tuong <ttruong2621@sdsu.edu>
2026-08-27 19:33:28 -0700

    Fixing second header show up after footer in all (auth) page

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -64,7 +64,6 @@ export default function AuthLayout({children}: {children: React.ReactNode}){
         <>
             <DashboardProvider>
                 <div className="min-h-screen flex flex-col">
-                    <Header />
                     <main className="flex-1">
                         {children}
                     </main>
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -34,8 +34,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}
+      <body className="min-h-full flex flex-col">
         < Header />
+        {children}
+        
         < Toaster />
       </body>
     </html>
