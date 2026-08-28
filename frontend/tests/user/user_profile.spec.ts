df0457dd7f83b60208e4eef00420fcd1ccd337b8
Tuong <ttruong2621@sdsu.edu>
2026-08-28 13:03:39 -0700

    Finish modify test to use user-profile

--- frontend/tests/user/user_profile.spec.ts
+++ frontend/tests/user/user_profile.spec.ts
@@ -28,6 +28,32 @@ test("user profile allow user to update account information and delete user acco
             page.getByText("Account updated successfully")
         ).toBeVisible()
 
+        await page.getByRole("button", { name: "Log-Out"}).first().click()
+
+        await expect(page).toHaveURL(/\/$/)
+
+        await page.pause()
+        
+        await expect(page.getByRole("button", {name:"Log In"})).toBeVisible()
+
+        await page.getByRole("button", { name: "Log In"}).click()
+
+        await expect(page.getByRole("dialog")).toBeVisible()
+        
+        await expect(
+        page.getByRole("heading", { name: "Log in to your Account" })
+        ).toBeVisible()
+
+        await page.getByLabel("Email").fill("Another@email.com")
+        await page.getByLabel("Password").fill("87654321")
+        await page.getByRole("button", { name: "Log In" }).click()
+
+        await expect(page).toHaveURL(/dashboard/)
+        const token = await page.evaluate(() =>
+            localStorage.getItem("access_token"))
+        expect(token).not.toBeNull()
+        
+        await page.pause()
     } finally {
         await Delete(page, user)
     }
